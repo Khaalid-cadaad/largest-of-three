89ba52a76bb76536fafc7605c70b046a3208dfd2
unknown <khalidabdi3101@gmail.com>
2024-08-31 23:14:26 +0300

    Done exercise two

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+const num1 = 13
+const num2 = 11
+const num3 = 12
+
+if (num1 >= num2 && num1 >= num3){
+    console.log("the largest number is num3")
+}
